68d3b844b8311d5f1626c1bea007f73eb7e57b10
你的姓名 <you@example.com>
2026-02-05 19:17:41 +0800

    fix: update iOS AppDelegate for React Native Navigation 7.39.2

--- ios/LxMusicMobile/AppDelegate.mm
+++ ios/LxMusicMobile/AppDelegate.mm
@@ -1,42 +1,33 @@
 #import "AppDelegate.h"
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
-
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  [ReactNativeNavigation bootstrapWithBridge:bridge];
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
+  self.moduleName = @"LxMusicMobile";
   self.initialProps = @{};
 
-  return YES;
-}
-
-- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge {
-  return [ReactNativeNavigation extraModulesForBridge:bridge];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   return [self getBundleURL];
 }
+
 - (NSURL *)getBundleURL
 {
 #if DEBUG
-  // 优先使用本地 bundle 文件（适用于 CI 构建）
-  NSURL *bundleURL = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-  if (bundleURL) {
-    return bundleURL;
-  }
-  // 如果没有本地 bundle，则使用开发服务器
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge {
+  return @[];
 }
 
 @end
